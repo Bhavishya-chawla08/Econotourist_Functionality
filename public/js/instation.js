c6ee71d7a98954b56a0125ba1dc3a4ffb1d7f975
Bhavishya-chawla08 <chawlabhavishya0@gmail.com>
2024-06-13 19:06:19 +0530

    Token Change

--- public/js/instation.js
+++ public/js/instation.js
@@ -1,6 +1,7 @@
 // Mapbox Public Access Key
 // ***************************** Access Token for Map ******************************************
- mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaDIwMDQiLCJhIjoiY2x2aTY5c3c2MWJxMDJrbzVxbWl0dG90OCJ9.I3pZLAZHCJaPR8GCDzQEkQ';
+//  mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaDIwMDQiLCJhIjoiY2x2aTY5c3c2MWJxMDJrbzVxbWl0dG90OCJ9.I3pZLAZHCJaPR8GCDzQEkQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaHlhMDgiLCJhIjoiY2x4ZDlzZ20zMDRlZTJuc2FlMWd5dzRkMCJ9.GL-kJpBA3WlRbh2cbHSmjg';
 
 // Initializing Map
 var map = new mapboxgl.Map({
--- public/js/train.js
+++ public/js/train.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   // ***************************** Access Token for Map ******************************************
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaDIwMDQiLCJhIjoiY2x2aTY5c3c2MWJxMDJrbzVxbWl0dG90OCJ9.I3pZLAZHCJaPR8GCDzQEkQ';
+    // mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaDIwMDQiLCJhIjoiY2x2aTY5c3c2MWJxMDJrbzVxbWl0dG90OCJ9.I3pZLAZHCJaPR8GCDzQEkQ';
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYmhhdmlzaHlhMDgiLCJhIjoiY2x4ZDlzZ20zMDRlZTJuc2FlMWd5dzRkMCJ9.GL-kJpBA3WlRbh2cbHSmjg';
         const map = new mapboxgl.Map({
           container: 'map',
           style: 'mapbox://styles/mapbox/streets-v11',
